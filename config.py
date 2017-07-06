05c15a8c1b77ce06a536b52ff8981b1053a46042
Li <palooneylee@gmail.com>
2017-07-06 17:57:31 -0500

    configuration update

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "B0ObhZGHXZdvdeGdGJRzsUp6B"
+consumer_secret = "20gvy4MDnDPFglqqWPoR7vFsfk5otB9lC6WzA0mBBdqHI9khwN"
+access_key = "140343853-OlHFgmgNZJwDfi1gXHDLhR5zmdjFvC3MVIvsMrLJ"
+access_secret = "cJKQWdJGVsib56vkGL18hO20x4KX5U56TEuHrmtYIWTJA"
