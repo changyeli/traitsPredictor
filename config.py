a3f7b7c4fec11543fbc65feb4c970bde1d56fc8c
Li <palooneylee@gmail.com>
2017-07-06 18:17:33 -0500

    update

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+# Tokens from Twitter
 consumer_key = "B0ObhZGHXZdvdeGdGJRzsUp6B"
 consumer_secret = "20gvy4MDnDPFglqqWPoR7vFsfk5otB9lC6WzA0mBBdqHI9khwN"
 access_key = "140343853-OlHFgmgNZJwDfi1gXHDLhR5zmdjFvC3MVIvsMrLJ"
