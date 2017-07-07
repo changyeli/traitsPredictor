31c30677acb7f1497b1f113b931f44858c0a461e
Li <palooneylee@gmail.com>
2017-07-07 17:48:27 -0500

    mayo tweet update

--- tweets.py
+++ tweets.py
@@ -1,24 +1,30 @@
-## load packages
-import re
-from bs4 import BeautifulSoup as bs 
-import urllib2
+import tweepy
 import csv
 
-## data inilization
-text = []
+consumer_key = 'B0ObhZGHXZdvdeGdGJRzsUp6B'
+consumer_secret = '20gvy4MDnDPFglqqWPoR7vFsfk5otB9lC6WzA0mBBdqHI9khwN'
+access_key = '140343853-OlHFgmgNZJwDfi1gXHDLhR5zmdjFvC3MVIvsMrLJ'
+access_secret = 'cJKQWdJGVsib56vkGL18hO20x4KX5U56TEuHrmtYIWTJA'
+totalTweets = []
 
-## functions
-def readHTML(user):	
-	my_page = urllib2.urlopen('https://twitter.com/' + str(user))
-	soup = bs(my_page, 'lxml')
-	return(soup)
+## get up to 3200 tweet blocks from an account 
+## inspired by: https://gist.github.com/yanofsky/5436496
+def getTweets(userName):
+	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_key, access_secret)
+	api = tweepy.API(auth)
+	temp = api.user_timeline(userName, count = 200)
+	totalTweets.extend(temp)
+	last = totalTweets[-1].id - 1
+	while len(temp) > 0:
+		temp = api.user_timeline(userName, count = 200, max_id = last)
+		totalTweets.extend(temp)
+		last = totalTweets[-1].id - 1
 
-def getText(soup):
-	for item in soup.findAll('p', attrs = {'class': 'TweetTextSize TweetTextSize--normal js-tweet-text tweet-text'}):
-		text.append(item.text.encode('utf-8'))
-
-
-soup = readHTML("MayoClinic")
-getText(soup)
-for item in text:
-	print item
+getTweets("MayoClinic")
+## extract tweet from blocks
+result = [tweet.text.encode('utf-8') for tweet in totalTweets]
+with open('tweet_mayo.csv', 'a') as f:
+	writer = csv.writer(f)
+	for row in result:
+		writer.writerow(row)
