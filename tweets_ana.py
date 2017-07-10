33685171bb6b0682257da4202fb2994361b15c58
Li <palooneylee@gmail.com>
2017-07-10 17:20:58 -0500

    reformat

--- tweets_ana.py
+++ tweets_ana.py
@@ -1,8 +1,8 @@
 ## load package
-import textblob
 import nltk
 import re
 from string import punctuation
+from nltk import word_tokenize
 
 ## remove all punctuation
 def strip_punctuation(s):
@@ -16,10 +16,13 @@ emoji_pattern = re.compile(
     u"(\ud83d[\ude80-\udeff])|"  # transport & map symbols
     u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
     "+", flags=re.UNICODE)
-## read file
-### every tweet 
-words = []
-with open('tweet_jimmyfallon.txt', 'r') as f:
-	words.extend(strip_punctuation(emoji_pattern.sub('', line)).split() for line in f)
-
-print words
+## tweet processing
+# stupid version, need to be changed
+def process(fileName):
+	words = []
+	with open(fileName, 'r') as f:
+		words.extend(strip_punctuation(emoji_pattern.sub('', line)).rstrip() for line in f)
+	token = [nltk.word_tokenize(i) for i in words]
+	return token
+token = process('tweet_jimmyfallon.txt')
+print token
