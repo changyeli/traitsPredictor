64f12f8d4a4677fdc21497203f162b87428b4726
Li <palooneylee@gmail.com>
2017-07-10 11:09:01 -0500

    reform and pull training data

--- tweets.py
+++ tweets.py
@@ -1,14 +1,17 @@
 import tweepy
-
+## auth info
 consumer_key = 'B0ObhZGHXZdvdeGdGJRzsUp6B'
 consumer_secret = '20gvy4MDnDPFglqqWPoR7vFsfk5otB9lC6WzA0mBBdqHI9khwN'
 access_key = '140343853-OlHFgmgNZJwDfi1gXHDLhR5zmdjFvC3MVIvsMrLJ'
 access_secret = 'cJKQWdJGVsib56vkGL18hO20x4KX5U56TEuHrmtYIWTJA'
-totalTweets = []
+## users and companies to scrape
+com_tweet = ["Padilla_Comm", "MayoClinic", "Twitter", "TheEconomist", "Google", "nytimes"]
+user_tweet = ["katyperry", "justinbieber", "BarackObama", "jimmyfallon", "EmmaWatson", "LeoDiCaprio", "HillaryClinton"]
 
 ## get up to 3200 tweet blocks from an account 
 ## inspired by: https://gist.github.com/yanofsky/5436496
 def getTweets(userName):
+	totalTweets = []
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_key, access_secret)
 	api = tweepy.API(auth)
@@ -19,11 +22,23 @@ def getTweets(userName):
 		temp = api.user_timeline(userName, count = 200, max_id = last)
 		totalTweets.extend(temp)
 		last = totalTweets[-1].id - 1
+	return totalTweets
 
-getTweets("Padilla_Comm")
-## extract tweet from blocks
-result = [tweet.text.encode('utf-8') for tweet in totalTweets]
-## write to .txt file
-with open('tweet_padilla.txt', 'w') as f:
-	for s in result:
-		f.write(s + '\n')
+## file processing
+def getTxt(screen):
+	result = []
+	s = "tweet_" + screen + '.txt'
+	getTweets(screen)
+	result = [tweet.text.encode('utf-8') for tweet in totalTweets]
+	with open(s, 'w') as f:
+		for item in result:
+			f.write(item + '\n')
+
+## get company tweets
+for com in com_tweet:
+	totalTweets = getTweets(com)
+	getTxt(com)
+## get user tweets
+for user in user_tweet:
+	totalTweets = getTweets(user)
+	getTxt(user)
