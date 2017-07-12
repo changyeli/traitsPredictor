a935a543f6b826941f8dff0c80751f2cbcefcb6c
Li <palooneylee@gmail.com>
2017-07-12 17:51:27 -0500

    pulling data from twitter

---
+++ fileProcess.py
@@ -0,0 +1,48 @@
+## import package
+import tweepy
+import re
+class fileProcess:
+ 	def __init__(self):
+ 		self.consumer_key = 'B0ObhZGHXZdvdeGdGJRzsUp6B'
+ 		self.consumer_secret = '20gvy4MDnDPFglqqWPoR7vFsfk5otB9lC6WzA0mBBdqHI9khwN'
+ 		self.access_key = '140343853-OlHFgmgNZJwDfi1gXHDLhR5zmdjFvC3MVIvsMrLJ'
+ 		self.access_secret = 'cJKQWdJGVsib56vkGL18hO20x4KX5U56TEuHrmtYIWTJA'
+ 		## selected account for testing
+ 		self.user = ["katyperry", "ConanOBrien", "Oprah", "jimmyfallon", "EmmaWatson", "LeoDiCaprio", "BillGates", "taylorswift13", "rihanna"]
+ 		self.comm = ["Padilla_Comm", "MayoClinic", "Twitter", "TheEconomist", "Google", "nytimes"]
+ 		## database
+ 		self.total = []
+ 	def getAPI(self):
+ 		try: 
+ 			auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+ 			auth.set_access_token(self.access_key, self.access_secret)
+ 			api = tweepy.API(auth)
+ 		except:
+ 			print("Error: authentication failed")
+ 		return api
+ 	def getUserTweet(self, api):
+ 		for each in self.user:
+ 			total = []
+ 			doc = {}
+ 			emoji_pattern = re.compile(
+ 			u"(\ud83d[\ude00-\ude4f])|"  # emoticons
+    		u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
+    		u"(\ud83d[\u0000-\uddff])|"  # symbols & pictographs (2 of 2)
+    		u"(\ud83d[\ude80-\udeff])|"  # transport & map symbols
+    		u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
+    		"+", flags=re.UNICODE)
+ 			temp = api.user_timeline(each, count = 200)
+ 			total.extend(temp)
+ 			last = total[-1].id - 1
+ 			while len(temp) > 0:
+ 				temp = api.user_timeline(each, count = 200, max_id = last)
+ 				total.extend(temp)
+ 				last = total[-1].id - 1
+ 			result = [emoji_pattern.sub(r'', tweet.text).encode('utf-8') for tweet in total]
+ 			doc[each] = result
+ 			self.total.append(doc.copy())
+ 			print len(self.total)
+
+x = fileProcess()
+api = x.getAPI()
+x.getUserTweet(api)
