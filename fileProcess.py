0f66a54460cabae3b33e2cd151cae3f649d38ce9
Li <palooneylee@gmail.com>
2017-07-13 11:47:37 -0500

    data pulling update

--- fileProcess.py
+++ fileProcess.py
@@ -10,8 +10,6 @@ class fileProcess:
  		## selected account for testing
  		self.user = ["katyperry", "ConanOBrien", "Oprah", "jimmyfallon", "EmmaWatson", "LeoDiCaprio", "BillGates", "taylorswift13", "rihanna"]
  		self.comm = ["Padilla_Comm", "MayoClinic", "Twitter", "TheEconomist", "Google", "nytimes"]
- 		## database
- 		self.total = []
  	def getAPI(self):
  		try: 
  			auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
@@ -23,7 +21,6 @@ class fileProcess:
  	def getUserTweet(self, api):
  		for each in self.user:
  			total = []
- 			doc = {}
  			emoji_pattern = re.compile(
  			u"(\ud83d[\ude00-\ude4f])|"  # emoticons
     		u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
@@ -39,10 +36,37 @@ class fileProcess:
  				total.extend(temp)
  				last = total[-1].id - 1
  			result = [emoji_pattern.sub(r'', tweet.text).encode('utf-8') for tweet in total]
- 			doc[each] = result
- 			self.total.append(doc.copy())
- 			print len(self.total)
+ 			s = "tweet_" + each + ".txt"
+ 			with open(s, "w") as f:
+ 				for item in result:
+ 					f.write(item + "\n")
+ 			print("finish " + each + "'s tweet scraping.")
+ 		for each in self.comm:
+ 			total = []
+ 			emoji_pattern = re.compile(
+ 			u"(\ud83d[\ude00-\ude4f])|"  # emoticons
+    		u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
+    		u"(\ud83d[\u0000-\uddff])|"  # symbols & pictographs (2 of 2)
+    		u"(\ud83d[\ude80-\udeff])|"  # transport & map symbols
+    		u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
+    		"+", flags=re.UNICODE)
+ 			temp = api.user_timeline(each, count = 200)
+ 			total.extend(temp)
+ 			last = total[-1].id - 1
+ 			while len(temp) > 0:
+ 				temp = api.user_timeline(each, count = 200, max_id = last)
+ 				total.extend(temp)
+ 				last = total[-1].id - 1
+ 			result = [emoji_pattern.sub(r'', tweet.text).encode('utf-8') for tweet in total]
+ 			s = "tweet_" + each + ".txt"
+ 			with open(s, "w") as f:
+ 				for item in result:
+ 					f.write(item + "\n")
+ 			print("finish " + each + "'s tweet scraping.")
+ 	## write tweets into file to save loading time		
+ 	
 
+## main function
 x = fileProcess()
 api = x.getAPI()
 x.getUserTweet(api)
