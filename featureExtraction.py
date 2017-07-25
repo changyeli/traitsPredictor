7e643fc020f0e1a8536736f8564431aabb4cfc7a
Li <palooneylee@gmail.com>
2017-07-25 15:19:53 -0500

    feature extraction update

--- featureExtraction.py
+++ featureExtraction.py
@@ -7,13 +7,14 @@ import nltk
 class featureExtraction:
 	def __init__(self):
 		self.feature = {} ## better format of feature vocabulary
-		self.userFeature = {} ## user word feature
+		self.userFeature = {} ## user word feature, with word as key, attributes as value
 		self.attr = [] ## the word list from feature, with order
 		self.root = "/Users/changye.li/Documents/scripts/traitsPredictor/process/"
 		self.better = "better.csv" ## better format feature
-		self.nrc = "/Users/changye.li/Documents/scripts/traitsPredictor/data/NRC.txt" ## feature vocabulary
+		self.nrc = "/Users/changye.li/Documents/scripts/traitsPredictor/data/NRC.txt" ## feature vocabulary file
 		self.docs = [] ## document filenames for character-seperate file
-		self.words = set()
+		self.words = set() ## feature vocabulary
+		self.allDocs = {} ## 
 	## feature vector with better format
 	def getFeature(self):
 		category = set()
@@ -37,10 +38,7 @@ class featureExtraction:
 			## form a dictionary
 			self.feature[item] = feature
 			self.attr = category
-		with open(self.root +self.better, "w") as f:
-			writer = csv.writer(f)
-			for k, v in self.feature.iteritems():
-				writer.writerow([k] + v)
+		
 	## read character files
 	def readFiles(self):
 		for r, d, f in os.walk(self.root):
@@ -54,10 +52,21 @@ class featureExtraction:
 				s = each[:-4]
 				## all words
 				row = f.readline().split(",")
+				## update user feature
+				## only keep words that appear in feature vocabulary
 				for item in row:
-					if item in self.words:
-						print "yes"
+					if item in self.feature:
+						if item in self.userFeature:
+							self.userFeature[item].append(self.feature[item])
+						else:
+							self.userFeature[item] = [self.feature[item]]
+				## integrate attributes
+				for k, v in self.userFeature.iteritems():
+					v = [sum(x) for x in zip(*v)]
+					print v
+				self.allDocs[s] = self.userFeature
 ## main function
 x = featureExtraction()
 x.readFiles()
 x.getFeature()
+x.extractFeature()
