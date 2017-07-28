cb2234f751d784a06b337f1f96e3e9200273b0a2
Li <palooneylee@gmail.com>
2017-07-28 10:09:19 -0500

    feature build finished

--- featureBuild.py
+++ featureBuild.py
@@ -12,7 +12,8 @@ class featureBuild:
 		self.root = "/Users/changye.li/Documents/scripts/traitsPredictor/clean/"
 		self.docs = [] ## file names under self.root
 		self.allData = pd.DataFrame() ## dataframe to store all matrices
-		self.attr = ['anticipation', 'joy', 'negative', 'sadness', 'disgust', 'positive', 'anger', 'surprise', 'fear', 'trust']
+		self.attr = ['anticipation', 'joy', 'negative', 'sadness', 'disgust', 'positive', 'anger', 'surprise', 'fear', 'trust'] ## attribute vocabulary
+		self.labels = [] ## cluster labels for all data
 	def readFile(self):
 		for r, d, f in os.walk(self.root):
 			for files in f:
@@ -25,7 +26,7 @@ class featureBuild:
 			## read file to data frame
 			df = pd.read_csv(path, delimiter = ",", names = self.attr)
 			## remove rows with all zeros
-			df = df[(df.T != 0).any()]
+			#df = df[(df.T != 0).any()]
 			self.allData = self.allData.append(df)
 	## create k-means for full matrix
 	## ruduce dimension using PCA, and plot the result of k-means
@@ -33,7 +34,9 @@ class featureBuild:
 		## reduce dimension using PCA
 		reduced = PCA(n_components = 2).fit_transform(self.allData)
 		## big-five style, using EM algorithm
-		km = KMeans(n_clusters = 5, algorithm = "full", max_iter = 1000).fit(reduced)
+		km = KMeans(n_clusters = 5, algorithm = "auto", max_iter = 1000).fit(reduced)
+		## store labels
+		self.labels = km.labels_.tolist()
 		## step size of the mesh
 		h = 0.2
 		## boundary
@@ -49,14 +52,23 @@ class featureBuild:
 		plt.plot(reduced[:, 0], reduced[:, 1], "k.", markersize = 2)
 		centroid = km.cluster_centers_
 		plt.scatter(centroid[:, 0], centroid[:, 1], marker = 'x', s = 169, linewidths = 3, color = 'w', zorder = 10)
-		plt.title('K-means clustering on the digits dataset (PCA-reduced data)\n'
+		plt.title('K-means clustering on the full matrices (PCA-reduced data)\n'
 		          'Centroids are marked with white cross')
 		plt.xlim(x_min, x_max)
 		plt.ylim(y_min, y_max)
 		plt.xticks(())
 		plt.yticks(())
 		plt.show()
+	## write full data and labels into files
+	def writeFile(self):
+		## write allData to file
+		self.allData.to_csv(self.root + "allData.csv", index = False, header = False)
+		with open(self.root + "labels.txt", "w") as f:
+			w = csv.writer(f, delimiter = "\n")
+			w.writerow(self.labels)
+
 x = featureBuild()
 x.readFile()
 x.scanData()
 x.fullKMeans()
+x.writeFile()
