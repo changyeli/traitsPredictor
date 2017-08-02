3effbf2219e68dd77b05da8ace768c962b0e0470
Li <palooneylee@gmail.com>
2017-08-02 14:08:12 -0500

    minor update

--- trainProcess.py
+++ trainProcess.py
@@ -12,7 +12,8 @@ class trainProcess:
 		self.path = "/Users/changye.li/Documents/scripts/traitsPredictor/process/better.csv"
 		self.root = "/Users/changye.li/Documents/scripts/traitsPredictor/train/"
 		self.docs = []
-		self.better = pd.DataFrame()
+		self.better = pd.DataFrame() ## to store data
+		self.vocal = {} ## reformat dataframe to dict
 		self.words = [] ## word to be analyzed
 		self.train = [] ## training data
 		self.test = [] ## test data
@@ -22,14 +23,18 @@ class trainProcess:
 			for files in f:
 				if files.endswith(".csv"):
 					self.docs.append(files)
-		self.better = pd.read_csv(self.path, header = None)
-		self.words = list(self.better[0])
+		self.better = pd.read_csv(self.path, names = ['voculabury', 'anticipation', 'joy', 'negative', 'sadness', 'disgust', 'positive', 'anger', 'surprise', 'fear', 'trust'])
+		self.words = list(self.better["voculabury"])
+		self.vocal = self.better.set_index("voculabury").T.to_dict("list")
+		print self.vocal
 	## scan and process full dataset
 	def processData(self):
 		data = pd.read_csv(self.root + "cAGR.csv")
 		status = list(data["STATUS"])
 		stop = set(stopwords.words("english"))
 		tokens = []
+		## predined column names
+		
 		for each in status:
 			## tokenize each status, remove all punctuations and lower the words
 			temp = [w for w in nltk.word_tokenize(each.translate(None, string.punctuation).lower()) if not w in stop]
@@ -37,9 +42,8 @@ class trainProcess:
 			x = [w for w in temp if w in self.words]
 			tokens.append(x)
 			## treat all empty lists as zeros
-		print tokens
 
 ## test
 x = trainProcess()
 x.readFiles()
-x.processAGR()
+x.processData()
