d0e652c83e8819ad71cef7295e9126e33cf211b9
Li <palooneylee@gmail.com>
2017-08-02 15:10:59 -0500

    training data feature build update

--- trainProcess.py
+++ trainProcess.py
@@ -2,7 +2,6 @@
 import os
 import sklearn
 import pandas as pd 
-import numpy as np 
 import nltk
 import string
 from nltk.corpus import stopwords
@@ -13,10 +12,11 @@ class trainProcess:
 		self.root = "/Users/changye.li/Documents/scripts/traitsPredictor/train/"
 		self.docs = []
 		self.better = pd.DataFrame() ## to store data
-		self.vocal = {} ## reformat dataframe to dict
+		self.vocal = {} ## reformat dataframe to dict, voculabury as key, attributes as values
 		self.words = [] ## word to be analyzed
 		self.train = [] ## training data
 		self.test = [] ## test data
+		self.matrix = [] ## matrix layout for training data
 	## read all .csv file
 	def readFiles(self):
 		for r, d ,f in os.walk(self.root):
@@ -26,7 +26,6 @@ class trainProcess:
 		self.better = pd.read_csv(self.path, names = ['voculabury', 'anticipation', 'joy', 'negative', 'sadness', 'disgust', 'positive', 'anger', 'surprise', 'fear', 'trust'])
 		self.words = list(self.better["voculabury"])
 		self.vocal = self.better.set_index("voculabury").T.to_dict("list")
-		print self.vocal
 	## scan and process full dataset
 	def processData(self):
 		data = pd.read_csv(self.root + "cAGR.csv")
@@ -34,16 +33,34 @@ class trainProcess:
 		stop = set(stopwords.words("english"))
 		tokens = []
 		## predined column names
-		
 		for each in status:
-			## tokenize each status, remove all punctuations and lower the words
+			## tokenize each status, remove all punctuations, stopwords and lower the words
 			temp = [w for w in nltk.word_tokenize(each.translate(None, string.punctuation).lower()) if not w in stop]
 			## only keep the words that appear in the NRC list
 			x = [w for w in temp if w in self.words]
 			tokens.append(x)
+		return tokens
 			## treat all empty lists as zeros
+	## reformat data into matrix layout, each status is a 10-dimensional vector, with adding each word's attribute to corresponding column
+	# input: pre-processed tokenized words (list of lists) from processData function
+	def getAttr(self, values):
+		## every status update
+		for each in values:
+			## feature vector
+			attr = [0] *10
+			## empty list
+			if len(each) == 0:
+				self.matrix.append(attr)
+			else:
+				## each word in a status update:
+				for item in each:
+					attr = [x + y for x, y in zip(attr, self.vocal[item])]
+				self.matrix.append(attr)
+		print self.matrix
+
 
 ## test
 x = trainProcess()
 x.readFiles()
-x.processData()
+tokens = x.processData()
+x.getAttr(tokens)
