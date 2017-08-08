4da16cb1b6a1eb91e0e75b12366f94a0ce17d685
Li <palooneylee@gmail.com>
2017-08-08 15:18:32 -0500

    origin cross validation backup

--- testing.py
+++ testing.py
@@ -11,28 +11,53 @@ with open('dict.csv', 'rb') as csv_file:
     mydict = dict(reader)
 
 
-## data processing
-def process(self):
-	## user ID
-	uid = set(self.data["AUTHID"].values.tolist())
-	tokens = []
-	## subset original dataset
-	k1 = self.data[self.data["AUTHID"] == "b7b7764cfa1c523e4e93ab2a79a946c4"]
-	s1 = k1["STATUS"].values.tolist()
-	#print k1.iloc[0] ## select row
-	for each in s1:
-		temp = [w for w in nltk.word_tokenize(each.translate(None, string.punctuation).lower())]
-		x = [w for w in temp if w in self.voca]
-		tokens.append(x)
-	print tokens
-	temp = []
-	for each in tokens:
-		attr = [0]*10
-		if len(each) == 0:
-			temp.append(attr)
-		else:
-			for item in each:
-				attr = [x + y for x, y in zip(attr, self.dic[item])]
-			temp.append(attr)
-	print temp
-	print [sum(x) for x in zip(*temp)]
+## train models, file by file
+	def trainModel(self, filename, per):
+		
+		## randomly select train and test data
+		train = self.matrix.sample(frac = per, replace = False)
+		train_index = list(train.index)
+		test_index = list(set(list(range(9917))) - set(train_index))
+		test = pd.DataFrame(self.matrix, index = test_index)
+		## retrieve class label 
+		labels = pd.read_csv(self.root + filename, usecols = [1])
+		labelTrain = pd.DataFrame(labels, index = train_index)
+		labelTest = pd.DataFrame(labels, index = test_index)
+		## dataframe to list
+		labelTrain = labelTrain.values.tolist()
+		labelTest = labelTest.values.tolist()
+
+		## model fitting
+		####################################
+		# MLP
+		clf = MLPClassifier(activation = "logistic", solver = "adam", 
+			alpha = 0.001, max_iter = 900000, hidden_layer_sizes = (150000, ))
+		clf.fit(train, labelTrain)
+		labelPredict = clf.predict(test)
+		## find the correct predictions
+		rate = [i for i, j in zip(labelPredict, labelTest) if i == j]
+		print "MLP correct rate: ", float(len(rate))/float(len(labelPredict))
+
+		#######################################
+		# SVM
+		clf1 = svm.NuSVC(kernel = "sigmoid", nu = 0.3)
+		clf1.fit(train, labelTrain)
+		labelPredict1 = clf1.predict(test)
+		rate1 = [i for i, j in zip(labelPredict1, labelTest) if i == j]
+		print "SVM correct rate: ", float(len(rate1))/float(len(labelPredict1))
+		
+		###########################################
+		# Bernoulli naive bayes
+		clf2 = BernoulliNB()
+		clf2.fit(train, labelTrain)
+		labelPredict2 = clf2.predict(test)
+		rate2 = [i for i, j in zip(labelPredict2, labelTest) if i == j]
+		print "Bernoulli Naive Bayes correct rate: ", float(len(rate2))/float(len(labelPredict2))
+		
+		###########################################
+		# KNN
+		clf3 = KNeighborsClassifier(n_neighbors = 10, weights = "distance")
+		clf3.fit(train, labelTrain)
+		labelPredict3 = clf3.predict(test)
+		rate3 = [i for i, j in zip(labelPredict3, labelTest) if i == j]
+		print "KNN correct rate: ", float(len(rate3))/float(len(labelPredict3))
