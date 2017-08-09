0a1c12b28416fc17af05b112f04c3cf613ef5556
Li <palooneylee@gmail.com>
2017-08-09 16:01:49 -0500

    feature analysis update

--- featureAna.R
+++ featureAna.R
@@ -1,23 +1,46 @@
 library(readr)
-library(ggplot2)
-library(MASS)
 library(leaps)
 library(car)
 ## load dataset
 processed_data = read_csv("~/Documents/scripts/traitsPredictor/processed_data.csv")
-processed_data = subset(processed_data, select = -c(X1))
 ## subset dataset 
 val = names(processed_data)[1:10]
 sext = processed_data[, 1:11]
 ## fit model
 sext1 = sext[1:200, ]
-full = lm(sext~., data = sext1)
-t = regsubsets(sEXT ~ ., data = sext1, nbest = 5)
+sext2 = sext[201:250, ]
+full = lm(sEXT~., data = sext1)
+t = regsubsets(sEXT ~ ., data = sext1, nbest = 10)
 plot(t)
-subsets(t, statistic = "adjr2")
-m1 = lm(sEXT~sadness + positive + surprise + fear, data = sext1)
-summary(m1)
-anova(m1)
-m2 = lm(sEXT ~ negative + sadness + positive + surprise + fear, data = sext1)
-summary(m2)
-anova(m1, m2)
+subsets(t, statistic = "adjr2", legend = FALSE)
+step(full, scale = 0, direction = "backward")
+m1 = lm(sEXT ~ positive + fear, data = sext1)
+m2 = lm(sEXT ~ sadness + positive + surprise + fear, data = sext1)
+m3 = lm(sEXT ~ sadness + positive + fear, data = sext1)
+m4 = lm(sEXT ~ sadness + disgust + positive + surprise + fear, data = sext1)
+m5 = lm(sEXT ~ negative + sadness + positive + surprise + fear, data = sext1)
+m6 = lm(sEXT ~ sadness + disgust + anger + surprise + fear, data = sext1)
+m7 = lm(sEXT ~ joy + negative + sadness + positive + surprise + fear, data = sext1)
+m8 = lm(sEXT ~ anger + joy + negative + sadness + positive + surprise + fear + trust, data = sext1)
+## calculate rMSE and MAE
+evaluate = function(actually, predicted){
+  error = actually - predicted
+  print(sqrt(mean(error)))
+  print(mean(abs(error)))
+}
+pre = predict(m1, newdata = sext2)
+evaluate(sext2$sEXT, pre)
+pre = predict(m2, newdata = sext2)
+evaluate(sext2$sEXT, pre)
+pre = predict(m3, newdata = sext2)
+evaluate(sext2$sEXT, pre)
+pre = predict(m4, newdata = sext2)
+evaluate(sext2$sEXT, pre)
+pre = predict(m5, newdata = sext2)
+evaluate(sext2$sEXT, pre)
+pre = predict(m6, newdata = sext2)
+evaluate(sext2$sEXT, pre)
+pre = predict(m7, newdata = sext2)
+evaluate(sext2$sEXT, pre)
+pre = predict(m8, newdata = sext2)
+evaluate(sext2$sEXT, pre)
