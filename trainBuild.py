82aff85f8ebbe6b055dead4ff546c61be0e50262
Li <palooneylee@gmail.com>
2017-08-14 11:15:21 -0500

    data output update;

---
+++ trainBuild.py
@@ -0,0 +1,80 @@
+## import packages
+import os
+import pandas as pd 
+import nltk
+import string
+import sklearn
+import numpy as np 
+from operator import add
+from nltk.corpus import stopwords
+from sklearn import preprocessing
+class trainBuild:
+	def __init__(self):
+		## path of vocabulary list in better format
+		self.path = "/Users/changye.li/Documents/scripts/traitsPredictor/process/better.csv"
+		## train dataset path
+		self.files = "/Users/changye.li/Documents/scripts/traitsPredictor/mypersonality_final.csv"
+		## data frame that contains all processed status
+		## each entry represents word count of each user
+		self.processed = pd.DataFrame()
+		## vocabulary list in better format, with attributes
+		self.better = pd.DataFrame()
+		## vocabulary list
+		self.voca = []
+		self.dic = {}
+		## all unprocessed data
+		self.data = pd.DataFrame()
+		## attribute name
+		self.attr = ['voculabury', 'anticipation', 'joy', 'negative', 'sadness', 'disgust', 'positive', 'anger', 'surprise', 'fear', 'trust']
+		self.values = ["sEXT", "sNEU", "sAGR", "sCON", "sOPN", "cEXT", "c0EU", "cAGR", "cCON", "cOPN"]
+	## read files
+	def readFiles(self):
+		self.better = pd.read_csv(self.path, names = self.attr)
+		self.voca = self.better["voculabury"].values.tolist()
+		self.dic = self.better.set_index("voculabury").T.to_dict("list")
+		self.data = pd.read_csv(self.files)
+	## data processing
+	def process(self):
+		process = []
+		## user ID
+		uid = set(self.data["AUTHID"].values.tolist())
+		stop = set(stopwords.words("english"))
+		## iterate each user
+		for id in uid:
+			## store each user's processed status update
+			tokens = []
+			## subset dataset
+			k1 = self.data[self.data["AUTHID"] == id]
+			t = k1[k1.columns[-10:]].iloc[0].values.tolist()
+			## retrieve user's status update
+			s1 = k1["STATUS"].values.tolist()
+			## iterate each status update 
+			for each in s1:
+				## remove punctuation
+				temp = [w for w in nltk.word_tokenize(each.translate(None, string.punctuation).lower()) if not w in stop]
+				## remove stopwords
+				x = [w for w in temp if w in self.voca]
+				tokens.append(x)
+			## store attributes of each user's processed status update
+			temp = []
+			## iterate each status update
+			for each in tokens:
+				attr = [0]*10
+				if len(each) == 0: ## empty list
+					temp.append(attr)
+				else:
+					## iterate each word in status
+					for item in each:
+						attr = [x + y for x, y in zip(attr, self.dic[item])]
+					temp.append(attr)
+			temp_scaled = preprocessing.normalize(temp)
+
+			## element-wise addtion among list of lists
+			process.append([sum(x) for x in zip(*temp_scaled)] + k1[k1.columns[-10:]].iloc[0].values.tolist())
+			## reformat into new and processed dataframe
+		process = pd.DataFrame(process, columns = self.attr[1:] + self.values)
+		process.to_csv("processed_data.csv", index = False)
+## test
+x = trainBuild()
+x.readFiles()
+x.process()
