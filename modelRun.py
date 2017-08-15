3eaebb5e51e9c3bb6bd479def9e4ed1d079a738d
Li <palooneylee@gmail.com>
2017-08-15 13:36:19 -0500

    model update

--- modelRun.py
+++ modelRun.py
@@ -1,5 +1,26 @@
 import os
 import csv
+import pandas as pd 
 class modelRun:
 	def __init__(self):
 		self.path = "/Users/changye.li/Documents/scripts/traitsPredictor/clean/"
+	## get the list of all docs 
+	# output: list that contains testing filenames
+	def getDocs(self):
+		docs = []
+		for r, d ,f in os.walk(self.path):
+			for files in f:
+				if files.endswith(".csv"):
+					docs.append(files)
+		return docs
+	## get attribute vector for each test user
+	# output: normalized dataframe, which each entry represents user's attribute
+	def processFiles(self):
+		docs = self.getDocs()
+		for each in docs:
+			df = pd.read_csv(self.path + each)
+
+
+## test
+x = modelRun()
+x.processFiles()
